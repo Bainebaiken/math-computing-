a445a56f45c217990ef3f2a94b28e66795075da6
nasejje mariam lumala <142495637+Bainebaiken@users.noreply.github.com>
2023-11-24 05:59:17 -0800

    mathmatics

---
+++ Mathematical computering/example    1/GROUP WORK/exponent.py
@@ -0,0 +1,17 @@
+# Simplifying Exponential Expressions
+
+# Example 1: 2^3
+result_1 = 2**3
+print(f"2^3 = {result_1}")
+
+# Example 2: (3^2)^4
+result_2 = (3**2)**4
+print(f"(3^2)^4 = {result_2}")
+
+# Example 3: 5^0
+result_3 = 5**0
+print(f"5^0 = {result_3}")
+
+# Example 4: (2^4) / (2^2)
+result_4 = (2**4) / (2**2)
+print(f"(2^4) / (2^2) = {result_4}")
